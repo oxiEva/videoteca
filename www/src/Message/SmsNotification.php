407cb3b40d65e8e90eb9e6196213beb712b70a09
oxiEva <eva.lujan@holaluz.com>
2020-08-06 10:25:44 +0200

    Full skeleton symfony 5

---
+++ www/src/Message/SmsNotification.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Message;
+
+class SmsNotification
+{
+    private $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
