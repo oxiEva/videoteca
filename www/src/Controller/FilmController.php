dc750084f0ac7de3e448099b4fb0fca11ae0be24
oxiEva <eva.lujan@holaluz.com>
2020-08-09 11:14:08 +0200

    Rename controller

--- www/src/Controller/FilmController.php
+++ www/src/Controller/FilmController.php
@@ -6,16 +6,15 @@ namespace App\Controller;
 use App\Entity\Film;
 use App\Repository\FilmRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class GhibliListController extends AbstractController
+final class FilmController extends AbstractController
 {
     /**
-     * @Route("/ghibli/list", name="ghibli_list")
+     * @Route("/film/list", name="film_list")
      */
 
     public function listAction(Request $request, FilmRepository $filmRepository)
@@ -48,7 +47,7 @@ final class GhibliListController extends AbstractController
     }
 
     /**
-     * @Route("/new/film", name="create_film")
+     * @Route("/film/new", name="create_film")
      * @param Request $request
      * @param EntityManagerInterface $em
      * @return JsonResponse
@@ -56,11 +55,11 @@ final class GhibliListController extends AbstractController
     public function createFilm(Request $request, EntityManagerInterface $em)
     {
         $film = new Film();
-        $film->setTitle('Castle in the Sky');
-        $film->setDescription('The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. ');
+        $film->setTitle('Spirited Away ');
+        $film->setDescription(' 10-year old Chihiro becomes trapped in a forbidden world of gods and magic when her parents take her to investigate the other side of the tunnel.');
 
         //$em->persist($film);
-        // $em->flush();
+        //$em->flush();
 
         $response = new JsonResponse();
         $response->setData(
