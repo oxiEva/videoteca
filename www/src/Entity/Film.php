17dd3fae045c53b1393bb3dbd4d2cb39f42a7455
Eva Lujan <eva.lujan@holaluz.com>
2020-09-13 11:47:30 +0200

    Create validation for description length

--- www/src/Entity/Film.php
+++ www/src/Entity/Film.php
@@ -8,6 +8,7 @@ use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
@@ -39,6 +40,12 @@ class Film
     /**
      * A little description for the film
      * @ORM\Column(type="string", length=512)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min=2,
+     *     max=500,
+     *     maxMessage="Describe the film in 500 chars or less"
+     * )
      * @Groups({"film_listing:read"})
      */
     private $description;
