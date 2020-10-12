6a129a6bfe90b62d2067bb2265012b9ef1b92003
Eva Lujan <eva.lujan@holaluz.com>
2020-10-12 11:38:00 +0200

    Messenger controller example

---
+++ www/src/Controller/MessengerController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use App\Message\SmsNotification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class MessengerController extends AbstractController
+{
+    public function index(MessageBusInterface $bus)
+    {
+        // will cause the SmsNotificationHandler to be called
+        $bus->dispatch(new SmsNotification('Look! I created a message!'));
+
+        // or use the shortcut
+        $this->dispatchMessage(new SmsNotification('Look! I created a message!'));
+
+        // ...
+    }
+}
