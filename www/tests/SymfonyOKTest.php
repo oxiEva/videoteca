bb4684985abc38b8e400ee9eb3c27e89a60f81f1
oxiEva <eva.lujan@holaluz.com>
2020-11-21 11:16:55 +0100

    Symfony is working test

---
+++ www/tests/SymfonyOKTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SymfonyOkTest extends WebTestCase
+{
+    public function testSymfonyIsOnline()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
