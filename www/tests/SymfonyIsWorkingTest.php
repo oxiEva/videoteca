81231dc4a66e4bd46b809009b5e62df3a8cced4f
oxiEva <eva.lujan@holaluz.com>
2020-12-04 18:22:44 +0100

    Add executable vendor to bin

--- www/tests/SymfonyIsWorkingTest.php
+++ www/tests/SymfonyIsWorkingTest.php
@@ -1,12 +1,11 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class SymfonyOkTest extends WebTestCase
+final class SymfonyIsWorkingTest extends WebTestCase
 {
     public function testSymfonyIsOnline()
     {
